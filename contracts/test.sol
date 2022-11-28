1288a96410af8ef4edb0bcbfc73e82364aedb95a
RedHeadphone <huzaifa.k@somaiya.edu>
2022-11-29 00:05:19 +0530

    sending ETH script

---
+++ contracts/test.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+
+contract Test {
+    receive() external payable {   
+    }
+    // fallback() external payable {
+    // }
+}
---
+++ scripts/send.js
@@ -0,0 +1,24 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+
+    const addr = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+    const value = "1.0"
+
+    const [owner, addr1, addr2] = await hre.ethers.getSigners();
+    owner.sendTransaction({from: owner.address, to: addr, value: hre.ethers.utils.parseEther(value)});
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
